2e8e95d3accb48c65532e51674c902d55a9095be
Michael Gorman <michaelgorman@crc-dot1x-nat-10-239-108-75.bu.edu>
2025-10-22 22:08:02 -0400

    Last implementation

--- src/components/mains/Hobbies.tsx
+++ src/components/mains/Hobbies.tsx
@@ -1,12 +1,11 @@
 import {MainContainer, StyledDiv, StyledH2, StyledImg, StyledP} from "../styles/MainStyles.tsx";
-import img02 from "../../assets/me.jpg"
 
 export default function Hobbies() {
     return (
         <MainContainer>
             <StyledH2> Hobbies </StyledH2>
             <StyledDiv>
-                <StyledImg src={img02} alt="Michael"/>
+                <StyledImg src="src/assets/Race.PNG" alt="Michael"/>
                 <StyledP>One of my favorite hobbies is spending time with my two dogs, an English Lab and an English Bulldog,
                     both of whom love going on walks and playing outside. I have a
                     deep love for the outdoors, whether it’s enjoying fresh air on a quiet walk, running under the open
